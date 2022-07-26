d2e890af645fe61d01428d1ab3873d17dfe704f5
Yura <yakor_101@mail.ru>
2022-07-26 20:47:39 +1000

    second commit

--- app/src/main/java/ru/yakovlev/notesappmvvm/navigation/NotesNavHost.kt
+++ app/src/main/java/ru/yakovlev/notesappmvvm/navigation/NotesNavHost.kt
@@ -1,31 +1,29 @@
 package ru.yakovlev.notesappmvvm.navigation
 
-import android.provider.ContactsContract
-import androidx.compose.foundation.interaction.DragInteraction
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import ru.yakovlev.notesappmvvm.screens.Add
-import ru.yakovlev.notesappmvvm.screens.Main
-import ru.yakovlev.notesappmvvm.screens.Note
-import ru.yakovlev.notesappmvvm.screens.Start
+import ru.yakovlev.notesappmvvm.screens.AddScreen
+import ru.yakovlev.notesappmvvm.screens.MainScreen
+import ru.yakovlev.notesappmvvm.screens.NoteScreen
+import ru.yakovlev.notesappmvvm.screens.StartScreen
 
-sealed class NavRoute(val route: String){
-    object Start: NavRoute("start_screen")
-    object Main: NavRoute("main_screen")
-    object Add: NavRoute("add_screen")
-    object Note: NavRoute("note_screen")
+sealed class NavRoute(val route: String) {
+    object Start : NavRoute("start_screen")
+    object Main : NavRoute("main_screen")
+    object Add : NavRoute("add_screen")
+    object Note : NavRoute("note_screen")
 }
 
 @Composable
 fun NotesNavHost() {
     val navController = rememberNavController()
-    
-    NavHost(navController = navController, startDestination = NavRoute.Start.route){
-        composable(NavRoute.Start.route){ Start(navController = navController) }
-        composable(NavRoute.Main.route){ Main(navController = navController) }
-        composable(NavRoute.Add.route){ Add(navController = navController) }
-        composable(NavRoute.Note.route){ Note(navController = navController) }
+
+    NavHost(navController = navController, startDestination = NavRoute.Start.route) {
+        composable(NavRoute.Start.route) { StartScreen(navController = navController) }
+        composable(NavRoute.Main.route) { MainScreen(navController = navController) }
+        composable(NavRoute.Add.route) { AddScreen(navController = navController) }
+        composable(NavRoute.Note.route) { NoteScreen(navController = navController) }
     }
 }
